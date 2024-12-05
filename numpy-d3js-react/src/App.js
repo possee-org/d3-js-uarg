910dc0fc00b06b6ef9a7b34db0c1b91bbd82dc51
user <ulises.a.rivero.garcia@gmail.com>
2024-12-05 13:54:17 -0600

    Added the files of the React version of the project. Including the exercise of the tutorial of React with D3

---
+++ numpy-d3js-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Barchart from './components/barchart';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello React + D3 world!</h1>
+    <Barchart/>
+    </div>
+  );
+}
+
+export default App;
